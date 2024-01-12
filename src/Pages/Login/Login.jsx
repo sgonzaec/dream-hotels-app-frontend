ff33b43c335c87be3ec24bfc8da0f86756d31b74
Santiago Tonguino <santiagotq2@gmail.com>
2024-01-12 13:05:03 -0500

    fix: Updapte login style

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -13,27 +13,27 @@ const Login = () => {
   const onSubmit = (data) => console.log(data);
   return (
     <div className="gradient">
-    <div className="cart">
-      <div className="Login">
-        <h1>Welcome to Dream Hotel</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="contentInput">
-            <input placeholder="Tonguiino" {...register("user", { required: true })} />
-            {errors.user && <span>Este campo es obligatorio</span>}
-          </div>
-          <div className="contentInput">
-            <input placeholder="Contraseña" type="password" {...register("password", { required: true})} />
-            {errors.password && <span>Este campo es obligatorio</span>}
-          </div>
+      <div className="cart login">
+        <div className="Login">
+          <h1>Welcome to Dream Hotel</h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="contentInput">
+              <input placeholder="Tonguiino" {...register("user", { required: true })} />
+              {errors.user && <span>Este campo es obligatorio</span>}
+            </div>
+            <div className="contentInput">
+              <input placeholder="Contraseña" type="password" {...register("password", { required: true })} />
+              {errors.password && <span>Este campo es obligatorio</span>}
+            </div>
 
-          <input id="submit" type="submit" value="Iniciar sesion" />
-        </form>
-        <div className="login">
-        <a href="../SingUp/">Olvidaste tu contraseña?</a>
-        <a href="#">No tienes cuenta?</a>
+            <input id="submit" type="submit" value="Iniciar sesion" />
+          </form>
+          <div className="options">
+            <a href="#">Olvidaste tu contraseña?</a>
+            <a href="#">No tienes cuenta?</a>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
